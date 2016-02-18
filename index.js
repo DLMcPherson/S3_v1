69a303bf192dcce16137d08d5220fac6ef5c5b11
David McPherson <DavidMcP555@gmail.com>
2016-02-18 09:33:39 -0800

    Added paths

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
 "use strict"
 
+// Setup objects
+  // Setup the window
 var renderer = PIXI.autoDetectRenderer(640, 480)
-renderer.backgroundColor = 0x222222
+var stage = new PIXI.Container()
+renderer.backgroundColor = 0xffffff
 renderer.roundPixels = true
-
+  // Setup the Firebase
 var firebase = new Firebase("https://ancaticipation.firebaseio.com")
+var graphics = new PIXI.Graphics();
+stage.addChild(graphics)
+
+var tick = 0
+var traj = 0
 
 // Rendering
 class Robot extends PIXI.Sprite {
   constructor(){
     super(PIXI.Texture.fromImage("SmallCookie.png"))
-    this.x = 0
-    this.y = 0
-    this.width = 100
-    this.height = 100
+    this.x = 50 ; this.y = 50
+    this.pivot.x = 150 ; this.pivot.y = 150
+    this.width = 100 ; this.height = 100
   }
   update(){
-    this.x += 1
-    if(this.x>640){
-      this.x=-100
-      this.y+=100
+    tick += 1
+    if(tick>=100){
+      // Increment trajectory counter
+      traj+=1
+      if(traj>9){
+        traj=0
+      }
+      // Print the trajectory counter descriptor
+      if(traj>=0 && traj<5){
+        console.log(traj,"Rightwards : ")
+      }
+      else{
+        console.log(traj,"Leftwards : ")
+      }
+      if(traj%5 == 0) {console.log("Standard")}
+      if(traj%5 == 1) {console.log("Avg. Offset")}
+      if(traj%5 == 2) {console.log("Keyframe")}
+      if(traj%5 == 3) {traj+=1}
+      if(traj%5 == 4) {console.log("Baseline")}
+      tick=0
     }
-    if(this.y>480)
-    {
-      this.y=0
-    }
+    // Update robot position to stay on track
+    this.x = 320+(myData.q[traj][0][tick]-5)*53
+    this.y = 430-myData.q[traj][1][tick]*53
   }
 }
 var robot = new Robot()
+stage.addChild(robot)
 
 var clock =  0
 var now = Date.now()
@@ -39,21 +62,34 @@ window.setInterval(function() {
   //console.log(clock)
   // Sprite management
   robot.update()
-  renderer.render(robot)
-},10)
+  // Path Drawing
+  graphics.clear()
+  graphics.lineStyle(5, 0x077f4d);
+  graphics.moveTo(320,430)
+  var i = 0
+  for(i=1; i<tick; i++){
+    graphics.lineTo(320+(myData.q[traj][0][i]-5)*53,430-myData.q[traj][1][i]*53)
+  }
+  // Rendering the stage
+  renderer.render(stage)
+},50)
 
+// Keyboard Listener
 document.addEventListener("keydown",function(event) {
-  console.log(clock,event.keyCode)
+  // Log time and key
   key = event.keyCode
+  console.log(clock,key)
+  // Record time and key to Firebase
   firebase.push({
     date : Date.now(),
     faveNumber : 5,
     keystroke : key,
     tlength : clock,
   })
+  // End
 })
 
 
-// ending stuff
+// Wrap it up
 var mount = document.getElementById("mount")
 mount.insertBefore(renderer.view, mount.firstChild)
