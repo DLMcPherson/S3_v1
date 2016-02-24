02f6afb2ff4c364a7b5509ab118e8eab95a7059c
David McPherson <DavidMcP555@gmail.com>
2016-02-24 08:12:42 -0800

    added tutorial/introduction

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
 "use strict"
 
-// ======================= SETUP PHASE ========================== //
-  // Setup the renderer
+// Setup the renderer
 var renderer = PIXI.autoDetectRenderer(640, 640)
-var stage = new PIXI.Container()
 renderer.backgroundColor = 0xffffff
 renderer.roundPixels = true
-  // Connect to my Firebase
+
+// Connect to my Firebase
 var firebase = new Firebase("https://ancaticipation.firebaseio.com")
-  // Graphics object for my stage
+
+// ===================== SETUP SCREENS ================== //
+
+// Standard Screen
+var stage = new PIXI.Container()
+  // Graphics object for lines and squares and such...
 var graphics = new PIXI.Graphics();
 stage.addChild(graphics)
+  // Text
+var instr1 = new PIXI.Text('Press \'Q\' to guess Goal on Left',{font : '12px Roboto', fill : 0x077f4d});
+instr1.x = 0
+instr1.y = 500
+stage.addChild(instr1)
+var instr2 = new PIXI.Text('Press \'P\' to guess Goal on Right',{font : '12px Roboto', fill : 0x077f4d});
+instr2.x = 400
+instr2.y = 500
+stage.addChild(instr2)
+var timer = new PIXI.Text('+100',{font : '48px Roboto', fill : 0x077f4d, align: 'ceneter'});
+timer.x = 250
+timer.y = 550
+stage.addChild(timer)
+var text = new PIXI.Text('Time left :',{font : '24px Roboto', fill : 0x077f4d, align: 'ceneter'});
+text.x = 0
+text.y = 550
+stage.addChild(text)
 
-// Setup screens
   // Win screen
 var yay = new PIXI.Container()
-var yup = new PIXI.Text('Correct!',{font : '60px Roboto', fill : 0x077f4d});
-yup.x = 300
-yup.y = 300
+var yup = new PIXI.Text('Correct!',{font : '60px Roboto', fill : 0x077f4d, align: 'center'});
+yup.x = 220
+yup.y = 200
 yay.addChild(yup)
+
   // Lose screen
 var nay = new PIXI.Container()
-var nope = new PIXI.Text('WRONG!',{font : '60px Roboto', fill : 0xcf4c34});
-nope.x = 300
-nope.y = 300
+var nope = new PIXI.Text('WRONG!',{font : '60px Roboto', fill : 0xcf4c34, align: 'center'});
+nope.x = 220
+nope.y = 200
 nay.addChild(nope)
 
-// Globals
-var tick = 0
-var traj = 0
+// Starting screen
+var helpStart = new PIXI.Container()
+var help = new PIXI.Text('The robot wants you to guess correctly, so it\'s going to try \ngiving different hints in different ways. Guess as soon as \nyou feel confident you know which goal it\'s heading for!',{font : '24px Roboto', fill : 0x077f4d, align: 'center'});
+help.x = 0
+help.y = 0
+helpStart.addChild(help)
+var next = new PIXI.Text('Ready? Press any key to continue.',{font : '24px Roboto', fill : 0xcf4c34, align: 'center'});
+next.x = 200
+next.y = 400
+helpStart.addChild(next)
 
+var tLindex = 0
+var trajList = [3,2,7,6,8,1]
+//var trajList = [3,3,2,3,7,6,8,1]
 // Robot class
 class Robot extends PIXI.Sprite {
   constructor(){
@@ -42,9 +73,21 @@ class Robot extends PIXI.Sprite {
     tick += 1
     if(tick>=100){
       // Increment trajectory counter
-      traj+=1
-      if(traj>9)
-        traj=0
+      if(mode==-4){
+        if(traj==0){
+          traj=5
+        }
+        else{
+          traj=0
+        }
+      }
+      if(mode==0){
+        tLindex+=1
+        if(tLindex>=6){
+          tLindex=0
+        }
+        traj=trajList[tLindex]
+      }
       // Print the trajectory counter descriptor
       if(traj>=0 && traj<5){
         console.log(traj,"Rightwards : ")
@@ -64,41 +107,62 @@ class Robot extends PIXI.Sprite {
     this.y = 430-myData.q[traj][1][tick]*53
   }
 }
+
+
+// Globals
+var tick = 0
+var traj = 0
+var mode = -1
+var modeExit = 10
+
 var robot = new Robot()
 stage.addChild(robot)
 
-// Text
-var instr1 = new PIXI.Text('Press \'Q\' to guess Goal on Left',{font : '12px Roboto', fill : 0x077f4d});
-instr1.x = 0
-instr1.y = 500
-stage.addChild(instr1)
-var instr2 = new PIXI.Text('Press \'P\' to guess Goal on Right',{font : '12px Roboto', fill : 0x077f4d});
-instr2.x = 400
-instr2.y = 500
-stage.addChild(instr2)
-var timer = new PIXI.Text('+100',{font : '48px Roboto', fill : 0x077f4d, align: 'ceneter'});
-timer.x = 250
-timer.y = 550
-stage.addChild(timer)
-
 // Render Loop
-var clock =  0
-var timeleft = 100
-var now = Date.now()
-var key = null
-var mode = 0
-var modeExit = 10
+var clock =  0 ; var now = Date.now()
 window.setInterval(function() {
   // Time management
   clock += Date.now() - now
   now = Date.now()
   // Trajectory screen
-  if(mode==0){
+  if(mode==-1){
     // Sprite management
-    robot.update()
+    //robot.update()
+    // Decrease timer
+    timer.text = ''
+    text.text = 'This is our robot'
+    instr1.text = 'Press any key to advance'
+    instr2.text = 'Press any key to advance'
+    tick=-1
+  }
+  if(mode==-2){
+    text.text = 'It will either: move to the goal on the left...'
+    traj=5
+  }
+  if(mode==-3){
+    text.text = '...or move to the goal on the right'
+    traj=0
+  }
+  if(mode==-4){
+    text.text = 'Guess which goal it\'s heading for ahead \n of time to gain points!'
+    instr1.text = 'Press \'Q\' key to guess Goal on Left'
+    instr2.text = 'Press \'P\' key to guess Goal on Right'
+
+    instr1.style = {font : '14px Roboto', fill : 0xcf4c34}
+    instr2.style = {font : '14px Roboto', fill : 0xcf4c34}
+    if(correctQ==1 && correctP==1){
+      mode=3
+    }
+  }
+  if(mode==0){
     // Decrease timer
     var temp = 100 - tick
+    text.text = 'Time left : '
     timer.text = temp.toString()
+  }
+  if(mode<1){
+    // Sprite management
+    robot.update()
     // Path Drawing
     graphics.clear()
     graphics.lineStyle(5, 0x077f4d);
@@ -126,14 +190,49 @@ window.setInterval(function() {
       mode=0
     }
   }
+  if(mode==3){
+    // Rendering the stage
+    instr1.style = {font : '12px Roboto', fill : 0x077f4d}
+    instr2.style = {font : '12px Roboto', fill : 0x077f4d}
+    renderer.render(helpStart)
+  }
+  if(mode==4){
+    // Rendering the stage
+    renderer.render(yay)
+    modeExit+=-1
+    if(modeExit==0){
+      mode=-4
+    }
+  }
+  if(mode==5){
+    // Rendering the stage
+    renderer.render(nay)
+    modeExit+=-1
+    if(modeExit==0){
+      mode=-4
+    }
+  }
 },50)
 
 // Keyboard Listener Loop
+var key = null
+var correctP = 0
+var correctQ = 0
 document.addEventListener("keydown",function(event) {
   // Log time and key
   key = event.keyCode
   console.log(clock,tick,key)
   var correct = 0
+  if(mode<0 && mode>-4){
+    mode-=1
+    tick=0
+  }
+  if(mode == 3){
+    mode=0
+    tick=0
+    tLindex=0
+    traj=trajList[tLindex]
+  }
   if(mode==0){
     if(key==81){
       if(traj>=5){
@@ -144,10 +243,8 @@ document.addEventListener("keydown",function(event) {
         correct=0
         mode=2
       }
-      modeExit=10
-      tick=100
     }
-    if(key==80){
+    else if(key==80){
       if(traj<5){
         correct=1
         mode=1
@@ -156,17 +253,45 @@ document.addEventListener("keydown",function(event) {
         correct=0
         mode=2
       }
-      modeExit=10
-      tick=100
     }
     // Record time and key to Firebase
     if(key==80 || key==81){
+      modeExit=10
+      tick=100
       firebase.push({
         date : Date.now(),
         correctness : correct,
+        ip : userip,
         keystroke : key,
         tlength : tick,
       })
+    }
+  }
+  if(mode==-4){
+    if(key==80 || key==81){
+      modeExit=10
+      tick=100
+      console.log(correctQ,correctP)
+    }
+    if(key==81){
+      if(traj>=5){
+        correctQ=1
+        mode=4
+      }
+      else {
+        correctQ=0
+        mode=5
+      }
+    }
+    else if(key==80){
+      if(traj<5){
+        correctP=1
+        mode=4
+      }
+      else {
+        correctP=0
+        mode=5
+      }
     }
   }
   // End
